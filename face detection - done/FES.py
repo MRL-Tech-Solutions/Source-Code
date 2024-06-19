2ef1e9a112856d438609bbc6d8c91c82f70bd7ff
MRL-Tech-Solutions <ipresearch7@gmail.com>
2024-06-19 16:46:43 +0530

    Add files via upload

---
+++ face detection - done/FES.py
@@ -0,0 +1,48 @@
+import cv2
+
+# Load the cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+# Initialize webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Extract the face ROI
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
+        # Detect eyes
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        # Detect smile
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.5, minNeighbors=20)
+        for (xx, yy, ww, hh) in smiles:
+            cv2.rectangle(roi_color, (xx, yy), (xx + ww, yy + hh), (0, 0, 255), 2)
+
+    # Display
+    cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+# Release the VideoCapture object
+cap.release()
+cv2.destroyAllWindows()
